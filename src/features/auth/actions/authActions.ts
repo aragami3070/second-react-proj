9813f57f11a35b753f966537e218ec1b26fd36a9
aragami3070 <smirnov17612@gmail.com>
2026-05-13 22:06:38 +0400

    feat(feautres-auth): add getMeAction

--- src/features/auth/actions/authActions.ts
+++ src/features/auth/actions/authActions.ts
@@ -1,4 +1,7 @@
+import { authApi } from "@/shared/api/authApi";
 import { useAppStore } from "@/shared/store";
+import { asyncHandler } from "@/shared/utils/asyncHandler";
+import { getErrorMessage } from "@/shared/utils/errorTemplateMessage";
 
 export const logoutAction = () => {
   if (typeof window !== "undefined") {
@@ -9,3 +12,17 @@ export const logoutAction = () => {
   const { clearUser } = useAppStore.getState();
   clearUser();
 };
+export const getMeAction = async () => {
+  const { setUser, clearUser, setError } = useAppStore.getState();
+
+  asyncHandler(
+    async () => {
+      const user = await authApi.getMe();
+      setUser(user.data);
+    },
+    (_) => {
+      clearUser();
+      setError("Сессия истекла. Зайдите заново");
+    }
+  )
+}
