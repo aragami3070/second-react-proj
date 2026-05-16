db81d4aa5a0c186b47f31e4201de516a30e5c9d5
aragami3070 <smirnov17612@gmail.com>
2026-05-16 18:45:05 +0400

    feat(shared-config): add navLinks const for NavBar

---
+++ src/shared/config/nav.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { label: "Главная", path: "/", isPrivate: false },
+  { label: "Профиль", path: "/profile", isPrivate: true },
+  { label: "Создать цитату", path: "/create-quote", isPrivate: true },
+  { label: "Лента цитат", path: "/quotes", isPrivate: true },
+  { label: "Лучшее", path: "/random-quotes", isPrivate: true },
+];
