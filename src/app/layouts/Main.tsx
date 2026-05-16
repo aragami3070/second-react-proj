937404a0827ee6f7983a945dd6bf9be82a76f2db
aragami3070 <smirnov17612@gmail.com>
2026-05-16 19:57:13 +0400

    feat(app-layouts): add change theme logic in Main

--- src/app/layouts/Main.tsx
+++ src/app/layouts/Main.tsx
@@ -1,20 +1,42 @@
+"use client";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
-import { darkTheme } from '@/shared/theme/theme';
+import { darkTheme, lightTheme } from '@/shared/theme/theme';
 import { CommonWrapper } from '@/widgets/CommonWrapper/CommonWrapper';
+import { useEffect, useState } from 'react';
+import { ColorModeContext } from '@/shared/theme/types';
+import { CssBaseline } from '@mui/material';
+
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
+  // Загружаем из localStorage при старте, по умолчанию true
+  const [darkMode, setDarkMode] = useState(() => {
+    try {
+      const saved = localStorage.getItem('darkMode');
+      return saved ? JSON.parse(saved) : true;
+    } catch {
+      return true;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const toggleTheme = () => {
+    setDarkMode((prev: boolean) => !prev)
+  }
+
   return (
-    <html lang="ru">
-      <body>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={darkTheme}>
-            <CommonWrapper>
-              {children}
-            </CommonWrapper>
-          </ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
+    <AppRouterCacheProvider>
+      <ColorModeContext.Provider value={{ toggleTheme }}>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <CssBaseline />
+          <CommonWrapper>
+            {children}
+          </CommonWrapper>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </AppRouterCacheProvider>
   );
 }
