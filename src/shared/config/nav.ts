13a2c1c2231e777b9cca34600819fa20c467844c
aragami3070 <smirnov17612@gmail.com>
2026-05-16 21:01:34 +0400

    fix(shared-config): typos

--- src/shared/config/nav.ts
+++ src/shared/config/nav.ts
@@ -3,8 +3,8 @@ export const navLinks = [
   { label: "Профиль", path: "/profile", isPrivate: true },
   { label: "Создать цитату", path: "/create-quote", isPrivate: true },
   { label: "Лента цитат", path: "/quotes", isPrivate: true },
-  { label: "Лучшее", path: "/random-quotes", isPrivate: true },
+  { label: "Лучшее", path: "/random-quote", isPrivate: true },
 ];
 
-export const privateRoutes = ["/profile", "/create-quote", "/quotes", "/random-quotes"];
+export const privateRoutes = ["/profile", "/create-quote", "/quotes", "/random-quote"];
 export const guestRoutes = ["/login", "/register"];
