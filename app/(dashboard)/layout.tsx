1152bfda5939fbafcd873a75837d584e89fc98cb
aragami3070 <smirnov17612@gmail.com>
2026-05-16 19:55:38 +0400

    fix(app-routing-layout): move AppDashBoardLayout in RootLayout

--- app/(dashboard)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import AppDashboardLayout from '@/app/layouts/AppDashboard';
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <AppDashboardLayout>{children}</AppDashboardLayout>
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
+import AppDashboardLayout from '@/app/layouts/AppDashboard';
 import MainLayout from '@/app/layouts/Main';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <MainLayout>{children}</MainLayout>;
+  return (
+    <html lang="ru">
+      <body>
+        <MainLayout>
+          <AppDashboardLayout>
+            {children}
+          </AppDashboardLayout>
+        </MainLayout>
+      </body>
+    </html>
+  )
 }
