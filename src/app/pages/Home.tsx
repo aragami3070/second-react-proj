089d10845b0dd8a31a739aabd984d0b35b19dc3e
aragami3070 <smirnov17612@gmail.com>
2026-05-16 19:52:23 +0400

    feat(app-pages-home): add Img and LinkButton usage

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,41 +1,33 @@
-import { Button, SvgIcon, Typography } from "@mui/material";
-import Icon from "../assets/icon.svg?react"
+import { Typography } from "@mui/material";
 import { GridBackGroundLayout } from "@/shared/ui/GridBackGroundLayout";
 import { Img } from "@/shared/ui/Img";
-import Link from "next/link";
+import { LinkButton } from "@/shared/ui/LinkButton";
 
 export default function HomePage() {
   return (
     <>
       <GridBackGroundLayout sx={{ minWidth: '100vw' }}>
-        <SvgIcon component={Icon} inheritViewBox
+        <Img
+          src="/icon.svg"
           sx={{
             width: 100,
             height: 100,
             fontSize: 'inherit',
             flexShrink: 0,
             color: "#FFFFFF"
-          }} />
+          }}
+        />
         <Typography variant="h2" sx={{ padding: 2 }}>
           Добро пожаловать в &lt;Цитаты Дня&gt;!
         </Typography>
         <Typography variant="h4" sx={{ padding: 2, paddingBottom: 8 }}>
           Ваши любимые мемные цитаты из фильмов и сериалов.
         </Typography>
-        <Button
-          component={Link}
-          href="/profile"
-          sx={{
-            borderRadius: "12px",
-            px: 2.5,
-            boxShadow: 3,
-            color: "text.primary",
-            background: "primary.main"
-          }}>
+        <LinkButton
+          href="/profile" >
           Начать!
-        </Button>
+        </LinkButton>
       </GridBackGroundLayout>
-
 
       <GridBackGroundLayout sx={{ minWidth: '100vw' }}>
         <Typography variant="h2" sx={{ padding: 2 }}>
