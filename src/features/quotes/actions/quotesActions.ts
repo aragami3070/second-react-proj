7c087358e3ead0b4e3a15a041a214aaa92321c96
aragami3070 <smirnov17612@gmail.com>
2026-05-16 20:54:17 +0400

    feat(features-quotes): add fetch quotes count action

--- src/features/quotes/actions/quotesActions.ts
+++ src/features/quotes/actions/quotesActions.ts
@@ -1,10 +1,20 @@
 import { quotesApi } from "@/shared/api/quotesApi";
+import { useAppStore } from "@/shared/store";
 import { asyncHandler } from "@/shared/utils/asyncHandler";
 
 export const createQuoteAction = async (quoteText: string) => {
   return asyncHandler(
     async () => {
       await quotesApi.createQuote(quoteText);
+    },
+  )
+}
+
+export const fetchQuotesCountAction = async () => {
+  const { setTotal } = useAppStore.getState();
+  return asyncHandler(
+    async () => {
+      setTotal((await quotesApi.fetchQuotesCount()).data)
     },
   )
 }
