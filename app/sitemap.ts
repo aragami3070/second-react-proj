ac1d643e86102976f313b84a3d501bf66153bcaa
aragami3070 <smirnov17612@gmail.com>
2026-05-20 11:55:31 +0400

    feat(app): add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'http://localhost:3000';
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/login`,
+      lastModified: new Date(),
+      changeFrequency: 'never',
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/register`,
+      lastModified: new Date(),
+      changeFrequency: 'never',
+      priority: 0.5,
+    },
+  ];
+}
