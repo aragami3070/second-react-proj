19193e127babbb73368ea825ced56672726e7afc
aragami3070 <smirnov17612@gmail.com>
2026-05-20 11:57:46 +0400

    feat(app): add defualt metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,39 @@
 import AppDashboardLayout from '@/app/layouts/AppDashboard';
 import MainLayout from '@/app/layouts/Main';
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Цитаты Дня",
+    default: "Цитаты Дня - Твои любимые мемы",
+  },
+  description: "Лучшая коллекция мемных цитат из фильмов и сериалов.",
+  manifest: "/manifest.json",
+
+  openGraph: {
+    title: "Цитаты Дня",
+    description: "Лучшая коллекция мемных цитат",
+    url: "http://localhost:3000",
+    siteName: "Quotes of the Day",
+    images: [
+      {
+        url: "http://localhost:3000/og-image.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "ru_RU",
+    type: "website",
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      </head>
       <body>
         <MainLayout>
           <AppDashboardLayout>
